430c872a7b98e78c3b2770e4f98774a4666e79d7
KrisNathan <kristophernathanael2005@gmail.com>
2024-12-27 18:44:57 +0700

    fix(hono): serve all pages to indexhtml using serveStatic

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -2,10 +2,8 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { logger } from "hono/logger";
-import fs from "node:fs";
 
 const app = new Hono();
-const index = fs.readFileSync("../react/dist/index.html", "utf8");
 app.use("*", logger());
 app.use(
   "*",
@@ -13,11 +11,8 @@ app.use(
     root: "../react/dist",
   })
 );
-// for some reason path: doesnt want stuff outside of the project itself.
-// app.use("/*", serveStatic({ path: "../react/dist/index.html" }));
-app.get("/*", (c) => {
-  return c.html(index);
-});
+app.use("/*", serveStatic({ root: "../react/dist", path: "index.html" }));
+
 
 const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
